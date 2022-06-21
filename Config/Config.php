8ed27cd1b40b7b44c30a6aa8fa7eea9885544982
Coyaks <coyaks19@gmail.com>
2022-06-20 20:23:17 -0500

    first commit

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/SANDBOX/exampleCalendar/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "full_calendar";
+const charset = "charset=utf8";
+?>
